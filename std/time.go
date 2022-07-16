2883cf65cbbe549bb61a7b878d151bf95675194b
박상호 <hoyaspark@gmail.com>
2022-07-16 16:27:35 +0900

    feat : learning std pkg (time)

--- std/time.go
+++ std/time.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	duration := (2 * time.Hour) + (30 * time.Minute)
+	fmt.Println(duration)
+	time.AfterFunc(time.Second*3, func() {
+		fmt.Println("time.AfterFunc has close()")
+	})
+
+	time.Sleep(time.Second * 10)
+}
