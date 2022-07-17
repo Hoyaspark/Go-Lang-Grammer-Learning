fbb0a10439fa580e51ee279d5e40537256fd1f05
박상호 <hoyaspark@gmail.com>
2022-07-17 17:04:25 +0900

    feat : learning std pkg (json)

---
+++ README.md
@@ -0,0 +1,8 @@
+# Go 언어 정복기 - Learning Go
+
+---
+## 목표
+Go 언어의 모든것을 뜯어보고 사용해보자
+
+## 진행 상황
+![50%](https://progress-bar.dev/50)
--- std/json.go
+++ std/json.go
@@ -6,15 +6,57 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
 
 type Order struct {
 	ID          string        `json:"id"`
-	DateOrdered time.Time     `json:"date_ordered"`
+	DateOrdered RFC822ZTime   `json:"date_ordered"`
 	CustomerID  string        `json:"customer_id"`
 	Items       []interface{} `json:"items"`
+}
+
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type TestController struct {
+}
+
+func (tc *TestController) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	var buf Order
+
+	err := json.NewDecoder(r.Body).Decode(&buf)
+
+	fmt.Println(buf)
+
+	if err != nil {
+		log.Println(err)
+		rw.WriteHeader(500)
+		return
+	}
+
+	defer r.Body.Close()
+
+	json.NewEncoder(rw).Encode(&buf)
+
+	fmt.Println(buf)
+
+	//d := json.NewDecoder(r.Body)
+	//
+	//var bufArray []interface{}
+	//for d.More() {
+	//	var buf interface{}
+	//	d.Decode(&buf)
+	//
+	//	bufArray = append(bufArray, buf)
+	//}
+	//
+	//fmt.Println(bufArray)
+
 }
 
 type OrderController struct {
@@ -29,7 +71,7 @@ func (oc *OrderController) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	buf := make([]byte, 2048)
+	buf := make([]byte, 30)
 
 	n, err := r.Body.Read(buf)
 
@@ -68,8 +110,195 @@ func (oc *OrderController) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 }
 
+type Message struct {
+	Name string `json:"name"`
+	Text string `json:"text"`
+}
+
+func jsonStream() ([]Message, error) {
+	const testJson string = `
+	[
+		{"name": "Ed", "text": "Knock knock."},
+		{"name": "Sam", "text": "Who's there?"},
+		{"name": "Ed", "text": "Go fmt."},
+		{"name": "Sam", "text": "Go fmt who?"},
+		{"name": "Ed", "text": "Go fmt yourself!"}
+	]
+`
+
+	d := json.NewDecoder(strings.NewReader(testJson))
+
+	var result []Message
+
+	t, err := d.Token()
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	fmt.Println(t)
+
+	for d.More() {
+		var buf Message
+		err = d.Decode(&buf)
+
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+
+		result = append(result, buf)
+	}
+
+	t, err = d.Token()
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	fmt.Println(t)
+
+	return result, nil
+}
+
+//Custom Json Parsing
+type RFC822ZTime struct {
+	time.Time
+}
+
+func (rt RFC822ZTime) MarshalJSON() ([]byte, error) {
+	out := rt.Time.Format(time.RFC822)
+
+	return []byte("\"" + out + "\""), nil
+}
+
+func (rt *RFC822ZTime) UnmarshalJSON(b []byte) error {
+	t, err := time.Parse("\""+time.RFC822+"\"", string(b))
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	rt = &RFC822ZTime{t}
+
+	return nil
+}
+
+func customJSONParse() ([]Order, error) {
+	const jsonText = `
+[
+	{
+		"id":"12345",
+		"date_ordered":"2020-05-01T13:01:02Z",
+		"customer_id":"3",
+		"items":[{"id":"pedro","name":"Here"}]
+	},
+	{
+		"id":"12345",
+		"date_ordered":"2020-05-01T13:01:02Z",
+		"customer_id":"3",
+		"items":[{"id":"pedro","name":"Here"}]
+	},
+	{
+		"id":"12345",
+		"date_ordered":"2020-05-01T13:01:02Z",
+		"customer_id":"3",
+		"items":[{"id":"pedro","name":"Here"}]
+	},
+	{
+		"id":"12345",
+		"date_ordered":"2020-05-01T13:01:02Z",
+		"customer_id":"3",
+		"items":[{"id":"pedro","name":"Here"}]
+	}
+]
+`
+	d := json.NewDecoder(strings.NewReader(jsonText))
+
+	t, err := d.Token()
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	fmt.Println("token :", t)
+
+	var result []Order
+
+	for d.More() {
+		var buf Order
+		err = d.Decode(&buf)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		result = append(result, buf)
+	}
+
+	t, err = d.Token()
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	fmt.Println("token :", t)
+
+	return result, nil
+
+}
+
 func main() {
 
-	http.Handle("/order", &OrderController{})
+	//toFile := &Person{"pedro", 25}
+	//
+	//tmpFile, err := ioutil.TempFile(os.TempDir(), "sample-")
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//defer os.Remove(tmpFile.Name())
+	//
+	//err = json.NewEncoder(tmpFile).Encode(toFile)
+	//
+	//dec := json.NewDecoder(strings.NewReader("hello"))
+	//
+	//dec.More()
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//err = tmpFile.Close()
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//r, err := jsonStream()
+	//
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//
+	//fmt.Println(r)
+
+	//tmp := "2020-05-01T13:01:02Z"
+	//t, err := time.Parse(time.RFC3339, tmp)
+	//
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//
+	//s := t.Format(time.RFC822)
+	//fmt.Println(s)
+	http.Handle("/order", &TestController{})
 	http.ListenAndServe(":8080", nil)
 }
